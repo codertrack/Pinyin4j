8c4eddac7e4208fe27b9d7bd8d0cbeb807e7f9fe
wukai <wukai@wukai.com>
2016-10-19 22:51:40 +0800

    增加排序

---
+++ app/src/main/java/com/example/chenshuai/pinyin4j/PinCompare.java
@@ -0,0 +1,15 @@
+package com.example.chenshuai.pinyin4j;
+
+/**
+ * Created by wukai on 16/10/19.
+ */
+
+public class PinCompare implements Comparable<String> {
+
+	@Override
+	public int compareTo(String another) {
+		return 0;
+	}
+
+
+}
