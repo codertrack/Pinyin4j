8042ab58e42f4248322641df98b9c696bb58e120
wukai <wukai@wukai.com>
2016-10-20 11:51:27 +0800

    增加排序

---
+++ app/src/main/java/com/example/chenshuai/pinyin4j/IndexListview.java
@@ -0,0 +1,12 @@
+package com.example.chenshuai.pinyin4j;
+
+/**
+ * Created by wukai on 16/10/20.
+ */
+
+public class IndexListview {
+
+	public static void main(String[] args) {
+		System.out.println("1607棒棒哒");
+	}
+}
